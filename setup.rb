eaf688e01dee8bcbb8fc5904c369925f2ba54bb8
austin-hodges <hodgesaustinj@gmail.com>
2019-11-23 13:22:51 -0600

    -

--- setup.rb
+++ setup.rb
@@ -4,11 +4,13 @@ require 'atk_toolbox'
 if OS.is?("mac")
     system "brew install node"
     system "npm install --global parcel"
+    system "npm install"
 
 
 elsif OS.is?("windows")
     system "scoop install nodejs"
     system "npm install --global parcel"
+    system "npm install"
 else
     puts "Sorry this program does not know how to setupt on linux"
 
